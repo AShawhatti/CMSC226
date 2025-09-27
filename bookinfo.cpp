385e5643bb6801fd8eb7ceaafd3ae0ec9ac3849a
Adam Shawhatti <69728493+AShawhatti@users.noreply.github.com>
2025-09-27 14:06:09 -0400

    Update bookinfo.cpp

--- bookinfo.cpp
+++ bookinfo.cpp
@@ -1,8 +1,8 @@
 /*
  * Class: CMSC226
  * Instructor: Farnaz Eivazi
- * Description: Brief description of this file/module
- * Due: 9/21/2025
+ * Description: Gives information on the book like the title and the cost
+ * Due: 9/28/2025
  * I pledge that I have completed the programming assignment independently. 
  * I have not copied the code from a student or any source.
  * I have not given my code to any student.
@@ -13,17 +13,16 @@
 using namespace std;
 int main()
 {
-    //Self Explaining
-    int choice;
-    cout << "Serendipity Booksellers" << endl;
-    cout << "    Book Information" << endl;
-    cout << "ISBN: " << endl;
-    cout << "Title: " << endl;
-    cout << "Author: " << endl;
-    cout << "Publisher: " << endl;
-    cout << "Date Added: " << endl;
-    cout << "Quantity-On-Hand: " << endl;
-    cout << "Wholesale Cost: " << endl;
-    cout << "Retail Price: " << endl;
+    //Book Info 
+    cout << "Serendipity Booksellers\n";
+    cout << "    Book Information\n";
+    cout << "ISBN: \n";
+    cout << "Title: \n";
+    cout << "Author: \n";
+    cout << "Publisher: \n";
+    cout << "Date Added: \n";
+    cout << "Quantity-On-Hand: \n";
+    cout << "Wholesale Cost: \n";
+    cout << "Retail Price: \n";
     return 0;
 }
