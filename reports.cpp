5e10c24864b9e2659df1edbabfe56fb322972584
Adam Shawhatti <69728493+AShawhatti@users.noreply.github.com>
2025-09-27 14:04:09 -0400

    Add files via upload

---
+++ reports.cpp
@@ -0,0 +1,32 @@
+/*
+ * Class: CMSC226
+ * Instructor: Farnaz Eivazi
+ * Description: Brief description of this file/module
+ * Due: 9/21/2025
+ * I pledge that I have completed the programming assignment independently. 
+ * I have not copied the code from a student or any source.
+ * I have not given my code to any student.
+ * Print your Name here: Adam Shawhatti
+ */
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    //Self Explaining
+    int choice;
+    cout << "Serendipity Booksellers" << endl;
+    cout << "    Reports" << endl;
+    cout << "1. Inventory Listing" << endl;
+    cout << "2. Inventory Wholesale Value" << endl;
+    cout << "3. Inventory Retail Value" << endl;
+    cout << "4. Listing by Quantity" << endl;
+    cout << "5. Listing by Cost" << endl;
+    cout << "6. Listing by Age" << endl;
+    cout << "7. Return to the Main Menu" << endl;
+    cout << endl;
+    cout << "Enter your Choice: \n";
+    cin >> choice;
+    return 0;
+}
