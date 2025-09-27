12f4c6ad481e33f8a5922a3f8bf2822c6fec77eb
Adam Shawhatti <69728493+AShawhatti@users.noreply.github.com>
2025-09-27 13:30:26 -0400

    Add files via upload

---
+++ mainmenu.cpp
@@ -0,0 +1,29 @@
+/*
+ * Class: CMSC226
+ * Instructor: Farnaz Eivazi
+ * Description: Brief description of this file/module
+ * Due: 9/21/2025
+ * I pledge that I have completed the programming assignment independently. 
+ * I have not copied the code from a student or any source.
+ * I have not given my code to any student.
+ * Print your Name here: Adam Shawhatti
+ */
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    //Self Explaining
+    int choice;
+    cout << "Serendipity Booksellers" << endl;
+    cout << "       Main Menu" << endl;
+    cout << "1. Cashier Module" << endl;
+    cout << "2. Inventory Database Module" << endl;
+    cout << "3. Report Module" << endl;
+    cout << "4. Exit" << endl;
+    cout << " " << endl;
+    cout << "Enter your Choice: \n";
+    cin >> choice;
+    return 0;
+}
