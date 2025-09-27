7a7f48c374a660e77583d3fd72536e719bd592e8
Adam Shawhatti <69728493+AShawhatti@users.noreply.github.com>
2025-09-27 14:06:51 -0400

    Update mainmenu.cpp

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -1,8 +1,8 @@
 /*
  * Class: CMSC226
  * Instructor: Farnaz Eivazi
- * Description: Brief description of this file/module
- * Due: 9/21/2025
+ * Description: This is the main menu to go to other parts of the program depending on what you want to do
+ * Due: 9/28/2025
  * I pledge that I have completed the programming assignment independently. 
  * I have not copied the code from a student or any source.
  * I have not given my code to any student.
@@ -14,8 +14,9 @@
 using namespace std;
 int main()
 {
-    //Self Explaining
-    int choice;
+    //Input userchoice for ouput of where they want to go in main menu
+    int Mainmenuchoice;
+    do{
     cout << "Serendipity Booksellers" << endl;
     cout << "       Main Menu" << endl;
     cout << "1. Cashier Module" << endl;
@@ -24,6 +25,31 @@ int main()
     cout << "4. Exit" << endl;
     cout << " " << endl;
     cout << "Enter your Choice: \n";
-    cin >> choice;
+    cin >> Mainmenuchoice;
+
+    while(Mainmenuchoice < 1 || Mainmenuchoice > 4){
+        cout << "Invalid Choice" << endl;
+        cin >> Mainmenuchoice;
+    }
+    
+    switch(Mainmenuchoice){
+        case 1:
+            cout << "Cashier Module" << endl;
+            break;
+        case 2:
+            cout << "Inventory Database Module" << endl;
+            break;
+        case 3:
+            cout << "Report Module" << endl;
+            break;
+        case 4:
+            cout << "Exit" << endl;
+            break;
+    }
+    }while (Mainmenuchoice != 4);
+
+    
+
+    
     return 0;
 }
